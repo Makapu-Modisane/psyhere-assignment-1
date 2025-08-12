b1085f0611697be049dcec4be5ac8d97840a9e45
Makapu-Modisane <makapumodisane@gmail.com>
2025-08-12 21:02:37 +0200

    Add files via upload

---
+++ Ohm's_Law.py
@@ -0,0 +1,11 @@
+#Ohm's Law 
+print("Welcome to Ohm's law Voltage Calculations")
+
+I = int(input("Please enter the current value (in Amps):  "))
+R = int(input("Please enter the resistance value (in ohms): "))
+
+V = I * R
+print("The Formula for Ohm's Law is V = I x R ")
+
+
+print(f"The voltage  is : {V} volts ")
